cc3e2185cf8aa6eec3b7d3a804854bda111188f6
Venkata Krishna <venkata.krishna@kloudspot.com>
2023-05-15 13:06:56 +0530

    Initial Commit

---
+++ src/test/java/com/krish/communication/CommunicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krish.communication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
